5a62267eeac9bc473511b7aaaadfbc86feab7b52
svimaladhithan <vimaladhithan007@gmail.com>
2024-10-27 23:02:46 +0530

    feat: api updated

--- src/services/categoryApi.ts
+++ src/services/categoryApi.ts
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000/api/categories';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const fetchCategories = async (page: number) => {
-  const response = await axios.get(`${API_URL}?page=${page}`);
+  const response = await axios.get(`${API_URL}/categories?page=${page}`);
   return response.data;
 };
 
 export const fetchSelectedCategories = async (userId: string) => {
-  const response = await axios.get(`${API_URL}/selected/${userId}`);
+  const response = await axios.get(`${API_URL}/categories/selected/${userId}`);
   return response.data.selectedCategories || [];
 };
 
 export const updateSelectedCategories = async (userId: string, categoryIds: string[]) => {
-  await axios.post(`${API_URL}/select`, { userId, categoryIds });
+  await axios.post(`${API_URL}/categories/select`, { userId, categoryIds });
 };
